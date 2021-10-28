571ec796c259d00fb9c409e7b94359b048a48fb3
gsepulv1 <69608993+gsepulv1@users.noreply.github.com>
2021-10-28 10:10:26 -0500

    Update Lista_doblemente_enlazada.py

--- codigo/Lista_doblemente_enlazada.py
+++ codigo/Lista_doblemente_enlazada.py
@@ -76,8 +76,62 @@ class DoublyLinkedList:
             n = self.start_node
             while n is not None:
                 print(n.item , " ")
-                n = n.nref                
+                n = n.nref   
+                
+     #2.6 eliminar al comienzo               
+    def delete_at_start(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        self.start_node = self.start_node.nref
+        self.start_prev = None
+      # 2.7 eliminar al final
+     def delete_at_end(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        n.pref.nref = None
+        # elimimar elementos por valor
+          def delete_element_by_value(self, x):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return 
+        if self.start_node.nref is None:
+            if self.start_node.item == x:
+                self.start_node = None
+            else:
+                print("Item not found")
+            return 
 
+        if self.start_node.item == x:
+            self.start_node = self.start_node.nref
+            self.start_node.pref = None
+            return
+
+        n = self.start_node
+        while n.nref is not None:
+            if n.item == x:
+                break;
+            n = n.nref
+        if n.nref is not None:
+            n.pref.nref = n.nref
+            n.nref.pref = n.pref
+        else:
+            if n.item == x:
+                n.pref.nref = None
+            else:
+                print("Element not found")
+        
+        
 new_linked_list = DoublyLinkedList()  
 new_linked_list.insert_in_emptylist(50)   
 new_linked_list.insert_at_start(10)
